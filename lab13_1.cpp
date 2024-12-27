80b3a2f6c57175f9126353371808b801e5d87211
kasidin <kasidinkhamfoo@gmail.com>
2024-12-27 16:01:33 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,51 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double A[],int N, double B[]){
+    double sum;
+    double x2;
+    double mean;
+    double mean2;
+    
+    for(int i = 0; i<N; i++){
+    sum += A[i];
+    }
+    B[0]= sum/N;
+    mean = B[0];
+    for(int i = 0; i<N;i++)
+    {
+    x2+= pow(A[i]-mean,2);
+    }
+    mean2 = x2/N;
+    B[1]= pow(mean2,0.5);
+
+    double xi = 1;
+    double gm;
+    for(int i = 0; i<N; i++){
+        xi*=A[i];
+    }
+    gm = pow(xi,1.0/N);
+    B[2]= gm;
+
+    double altx;
+    for(int i = 0;i<N;i++){
+        altx+= 1/A[i];
+    }
+    B[3] = N/altx;
+
+    double max =A[0];
+    for(int i =1;i<N; i++){
+        if(A[i]>max){
+           max = A[i];
+        }
+    }
+    B[4]= max;
+
+    double min = A[0];
+    for(int i=1;i<N; i++){
+        if(A[i]< min){
+            min = A[i];
+        }
+    }
+    B[5]=min;
+}
